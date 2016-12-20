e3212ab1ea906d32f76483c778b51383a3b71005
alex-braun <braunacb@gmail.com>
2016-12-20 02:57:48 -0500

    Changed `localhost` to `127.0.0.1`.
    Possible source of error deployment error.

--- src/server/db/db.js
+++ src/server/db/db.js
@@ -1,7 +1,7 @@
 var mongoose = require('mongoose');
 
 // mongoose.connect('mongodb://localhost/todos');
-mongoose.connect(process.env.MONGOLAB_URI || 'mongodb://localhost/todos');
+mongoose.connect(process.env.MONGOLAB_URI || 'mongodb://127.0.0.1/todos');
 
 var Todo = mongoose.model('Todo', {
   task: String,
